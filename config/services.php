e4f769bcced556fef0754ea247364607d9b16603
CAPEL Maxime <83071634+fortyup@users.noreply.github.com>
2023-09-19 14:21:03 +0200

    Add dependency to Pokemon TCG API

--- config/services.php
+++ config/services.php
@@ -31,4 +31,11 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'pokemon-tcg' => [
+        'api_key' => env('POKEMON_TCG_API_KEY'),
+        'options' => [
+            'verify' => true,
+        ],
+    ],
+
 ];
