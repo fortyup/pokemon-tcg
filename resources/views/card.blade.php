8a1413554c1a1b77b17a6189c14474ac127b0de0
CAPEL Maxime <83071634+fortyup@users.noreply.github.com>
2023-09-26 10:43:55 +0200

    Refactor card view

--- resources/views/card.blade.php
+++ resources/views/card.blade.php
@@ -12,114 +12,112 @@
     <title>Pokemon</title>
 
     <!-- Afficher les données à partir de la fonction getCard(id) -->
-    <div class="card-details">
+    <div class="">
         <h1>Détails de la carte</h1>
-        <div class="card-container" style="display: flex">
-            <div class="card-image">
+        <div class="" style="display: flex">
+            <div class="">
                 <!-- Faire en sorte que l'image soit à 20% de sa taille originale -->
                 <img src="{{ $card['images']['large'] }}" alt="{{ $card['name'] }}" style="width: 310px; border-radius: 20px; box-shadow: 5px 5px 6px rgba(0, 0, 0, 0.45);">
             </div>
-            <div class="card-info" style="padding: 0.75rem; margin-left: 8%">
+            <div class="" style="padding: 0.75rem; margin-left: 8%">
                 <h2>Name: {{ $card['name'] }}</h2>
-                <p>Supertype: {{ $card['supertype'] }}</p>
+                <p>{{ $card['supertype'] }} - {{ implode(', ', $card['subtypes']) }}</p>
                 <!-- Si le pokemon a plusieurs sous-types, les afficher. Sinon n'afficher que le premier -->
-                @if(
-                    isset($card['subtypes']) &&
-                    is_array($card['subtypes']) &&
-                    count($card['subtypes']) > 0
-                )
-                    <p>Subtype: {{ implode(', ', $card['subtypes']) }}</p>
-                @endif
-                <p>HP: {{ $card['hp'] }}</p>
-                <!-- Si le pokemon a plusieurs types, les afficher. Sinon n'afficher que le premier -->
+                <p>HP {{ $card['hp'] }}</p>
+                <!-- Si le pokemon a plusieurs types, afficher les photo des types. Sinon n'afficher que le premier -->
                 @if(
                     isset($card['types']) &&
                     is_array($card['types']) &&
                     count($card['types']) > 0
                 )
-                    <p>Type: {{ implode(', ', $card['types']) }}</p>
+                    @foreach($card['types'] as $type)
+                        <img src="{{ asset('/images/type/'.$type.'.png') }}" alt="{{ $type }}" style="width: 25px;">
+                    @endforeach
                 @endif
                 <!-- Si le pokemon a une capacité en [0], l'afficher avec son type, son nom et son texte pareil pour une capacité en [1] sinon ne rienn afficher -->
                 @if(isset($card['abilities']) && is_array($card['abilities']) && count($card['abilities']) > 0)
-                    <p>Abilities:</p>
+                    <h2>Abilities:</h2>
                     <ul>
                         @foreach($card['abilities'] as $ability)
-                            <li>{{ $ability['type'] }}</li>
-                            <p>Name: {{ $ability['name'] }}</p>
-                            <p>Text: {{ $ability['text'] }}</p>
+                            <img src="{{ asset('/images/ability/'.$ability['type'].'.png') }}"
+                                 style="width: 100px; height: auto" alt="{{ $ability['type'] }}">
+                            <p>{{ $ability['name'] }}</p>
+                            <p>{{ $ability['text'] }}</p>
                         @endforeach
                     </ul>
                 @endif
+                <h2>Attacks:</h2>
                 <p>
-                    Attacks:
                 @if(isset($card['attacks']) && is_array($card['attacks']) && count($card['attacks']) > 0)
-                    <ul>
                         @foreach($card['attacks'] as $attack)
                             <li>{{ $attack['name'] }}</li>
                             <!-- Si le pokemon a plusieurs coûts d'attaque, les afficher. Sinon n'afficher que le premier -->
                             @if(isset($attack['cost']) && is_array($attack['cost']) && count($attack['cost']) > 0)
-                                <ul>
                                     @foreach($attack['cost'] as $cost)
-                                        <li>{{ $cost }}</li>
+                                    @if(isset($attack['cost']) && is_array($attack['cost']) && count($attack['cost']) > 0)
+                                        <img src="{{ asset('/images/type/'.$cost.'.png') }}" alt="{{ $cost }}"
+                                             style="width: 25px;">
+                @endif
                                     @endforeach
-                                </ul>
                             @endif
-                            <p>Damage: {{ $attack['damage'] }}</p>
-                            <p>Text: {{ $attack['text'] }}</p>
+                <p>{{ $attack['damage'] }}</p>
+                <p>{{ $attack['text'] }}</p>
                         @endforeach
-                    </ul>
                     @endif
                     </p>
                     <!-- Une carte peut avoir plusieurs règles. Afficher toutes les règles -->
                     @if(isset($card['rules']) && is_array($card['rules']) && count($card['rules']) > 0)
                         <p>Rules:</p>
-                        <ul>
                             @foreach($card['rules'] as $rule)
                                 <li>{{ $rule }}</li>
                             @endforeach
-                        </ul>
                     @endif
-                    <!-- Si la carte a une faiblesse, l'afficher -->
+                <!-- Si la carte a une faiblesse, afficher l'image du type -->
+                <div>
                     @if(isset($card['weaknesses']) && is_array($card['weaknesses']) && count($card['weaknesses']) > 0)
-                        <p>Weaknesses:</p>
-                        <ul>
-                            @foreach($card['weaknesses'] as $resistance)
-                                <li>{{ $resistance['type'] }}</li>
-                                <p>Value: {{ $resistance['value'] }}</p>
+                        <h2>Weakness:</h2>
+                        @foreach($card['weaknesses'] as $weaknesses)
+                            <img src="{{ asset('/images/type/'.$weaknesses['type'].'.png') }}"
+                                 alt="{{ $weaknesses['type'] }}" style="width: 25px;">
+                            <p>Value: {{ $weaknesses['value'] }}</p>
                             @endforeach
-                        </ul>
                     @else
-                        <p>Resistances: N/A</p>
+                        <h2>Weakness: N/A</h2>
                     @endif
                     <!-- Si la carte n'a pas de résistance, afficher N/A sinon afficher la valeur -->
                     @if(isset($card['resistances']) && is_array($card['resistances']) && count($card['resistances']) > 0)
-                        <p>Resistances:</p>
-                        <ul>
+                        <h2>Resistance:</h2>
                             @foreach($card['resistances'] as $resistance)
-                                <li>{{ $resistance['type'] }}</li>
+                            <img src="{{ asset('/images/type/'.$resistance['type'].'.png') }}"
+                                 alt="{{ $resistance['type'] }}" style="width: 25px;">
                                 <p>Value: {{ $resistance['value'] }}</p>
                             @endforeach
-                        </ul>
                     @else
-                        <p>Resistances: N/A</p>
+                        <h2>Resistance:</h2>
+                        <p>N/A</p>
                     @endif
                     <!-- Afficher tous les coûts de retraite du pokémon -->
                     @if(isset($card['retreatCost']) && is_array($card['retreatCost']) && count($card['retreatCost']) > 0)
-                        <p>Retreat Cost:</p>
-                        <ul>
+                        <h2>Retreat Cost:</h2>
                             @foreach($card['retreatCost'] as $cost)
-                                <li>{{ $cost }}</li>
+                            <img src="{{ asset('/images/type/'.$cost.'.png') }}" alt="{{ $cost }}"
+                                 style="width: 25px;">
                             @endforeach
-                        </ul>
                     @endif
                     <!-- Afficher la rareté de la carte -->
-                    <p>Rarity: {{ $card['rarity'] }}</p>
+                    <h2>Rarity:</h2>
+                    <p>{{ $card['rarity'] }}</p>
                     <!-- Afficher le numéro de la carte dans la série ex: 1 / 146 -->
-                    <p>Serie number: {{ $card['number'] }} / {{ $card['set']['printedTotal'] }}</p>
-                    <p>Artist: {{ $card['artist'] }}</p>
+                    <h2>Serie number:</h2>
+                    <p>{{ $card['number'] }} / {{ $card['set']['printedTotal'] }}</p>
+                    <h2>Artist:</h2>
+                    <p>{{ $card['artist'] }}</p>
                     <!-- Afficher le nom de l'extension de la carte ainsi que le symbole de l'extension -->
-                    <p>Set: {{ $card['set']['name'] }}</p>
-                    <img src="{{ $card['set']['images']['symbol'] }}" alt="{{ $card['set']['name'] }}" style="width: 25px;">
+                    <h2>Set:</h2>
+                        <a href="{{ route('set', ['id' => $card['set']['id']]) }}">
+                            <p>{{ $card['set']['name'] }}</p>
+                            <img src="{{ $card['set']['images']['symbol'] }}" alt="{{ $card['set']['name'] }}" style="width: 25px;">
+                        </a>
             </div>
         </div>
     </div>
