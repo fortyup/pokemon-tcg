07b3cfb5a73f9d3de817751df9f39bd6521bb54f
CAPEL Maxime <83071634+fortyup@users.noreply.github.com>
2023-09-26 17:10:03 +0200

    Update header and add style for sets page

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -5,26 +5,36 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Pokemon TCG</title>
+    @vite('resources/css/app.css')
 </head>
 <body>
-<header style="display: flex; align-items: center;">
-    <div style="width: 100%; display: flex; flex-direction: row; align-items: center;">
-        <a href="{{ route('index') }}" style="display: flex; align-items: center; width: 100%">
-            <img src="{{ asset('images/800px-Feuforêve-RFVF.png') }}" style="height: 50px; width: auto; margin-left: 5%; margin-right: 2%;" alt="Pokemon TGC logo">
-            <h1>Pokémon TCG Collection</h1>
-        </a>
-    </div>
-    <nav style="display: flex; flex-direction: row;">
-        <a href="{{ route('sets') }}" style="margin: 10px;">Sets</a>
-        <a href="{{ route('cards') }}" style="margin: 10px;">Cards</a>
+<header class="flex shadow-md w-full h-16">
+    <a href="{{ route('index') }}" class="flex items-center">
+        <img src="{{ asset('images/800px-Feuforêve-RFVF.png') }}" alt="Pokemon logo" class="mr-2 ml-2" style="width: 60px; height: auto">
+        <h1 class="text-lg">Pokémon TCG Collection</h1>
+    </a>
+    <nav class="flex items-center ml-auto mr-5">
+        <ul class="flex space-x-4">
+            <li><a href="{{ route('sets') }}" class="text-blue-500 hover:text-blue-700 mr-2">Sets</a></li>
+            <li><a href="{{ route('cards') }}" class="text-blue-500 hover:text-blue-700 mr-2">Cards</a></li>
+        </ul>
     </nav>
+    @if (Route::has('login'))
+        <div class="flex items-center mr-5">
+            @auth
+                <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
+            @else
+                <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
+
+                @if (Route::has('register'))
+                    <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
+                @endif
+            @endauth
+        </div>
+    @endif
 </header>
 
-
-@section('sidebar')
-@endsection
-
-    <div class="container">
+    <div class="container max-w-full p-12">
         @yield('content')
     </div>
 </body>
--- resources/views/sets.blade.php
+++ resources/views/sets.blade.php
@@ -1,30 +1,33 @@
 @extends('master')
 
 @section('content')
-        <h1>All sets</h1>
-        <div class="">
-            @foreach($sets as $set)
-                <div class="" href="{{ route('set', ['id' => $set['id']]) }}">
-                    <a href="{{ route('set', ['id' => $set['id']]) }}">
-                        <div class="" style="height: auto; width: auto">
-                            <img src="{{ $set['images']['logo'] }}" alt="{{ $set['name'] }}">
+    <h1 class="text-4xl">All sets</h1>
+    <div class="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-3 gap-4 h-full">
+        @foreach($sets as $set)
+            <div class="bg-white rounded-lg shadow-md p-4">
+                <a href="{{ route('set', ['id' => $set['id']]) }}">
+                    <div class="h-36 flex w-full justify-center items-center">
+                        <img src="{{ $set['images']['logo'] }}" alt="{{ $set['name'] }}"
+                             class="max-w-full max-h-full justify-center flex">
+                    </div>
+                    <div class="flex flex-row items-center p-4">
+                        <img src="{{ $set['images']['symbol'] }}" alt="{{ $set['name'] }}" class="w-12 h-12 mr-2">
+                        <div class="flex-column">
+                            <p class="text-3xl">{{ $set['name'] }}</p>
+                            <p>Released {{ $set['releaseDate'] }}</p>
                         </div>
-                        <div class="">
-                            <img src="{{ $set['images']['symbol'] }}" alt="{{ $set['name'] }}">
-                            <div>
-                                <p class="">{{ $set['name'] }}</p>
-                                <p>Released {{ $set['releaseDate'] }}</p>
-                            </div>
-                        </div>
+                    </div>
+                    <div class="pl-5">
                         @if(isset($set['legalities']) && is_array($set['legalities']))
                             @foreach($set['legalities'] as $legality => $value)
                                 @if($legality != 'unlimited')
-                                    <p>{{ $legality }} {{ $value }}</p>
+                                    <li>{{ $legality }} {{ $value }}</li>
                                 @endif
                             @endforeach
                         @endif
-                    </a>
-                </div>
-            @endforeach
-        </div>
+                    </div>
+                </a>
+            </div>
+        @endforeach
+    </div>
 @endsection
