f49aa0579faef412bcc7773511968c33b9ccbdb9
CAPEL Maxime <83071634+fortyup@users.noreply.github.com>
2023-10-05 11:00:53 +0200

    create card table

---
+++ database/migrations/2023_10_05_085113_create_card_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('card', function (Blueprint $table) {
+            $table->primary('id');
+            $table->string('id');
+            $table->string('name');
+            $table->string('supertype');
+            $table->string('level');
+            $table->string('hp');
+            $table->string('evolvesFrom');
+            $table->string('flavorText');
+            $table->string('number');
+            $table->string('artist');
+            $table->string('rarity');
+            $table->string('set_id');
+            $table->string('smallImage');
+            $table->string('largeImage');
+            $table->foreign('set_id')->references('id')->on('set');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('card');
+    }
+};
