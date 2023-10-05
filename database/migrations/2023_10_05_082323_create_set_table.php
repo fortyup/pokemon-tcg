c8293452b0fa0e9e9b1b92612fcd91c1dcd69214
CAPEL Maxime <83071634+fortyup@users.noreply.github.com>
2023-10-05 10:49:13 +0200

    create set table

---
+++ database/migrations/2023_10_05_082323_create_set_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('set', function (Blueprint $table) {
+            $table->primary('id');
+            $table->string('id');
+            $table->string('name');
+            $table->string('series');
+            $table->string('printedTotal');
+            $table->string('total');
+            $table->string('ptcgoCode');
+            $table->date('releaseDate');
+            $table->datetime('updatedAt');
+            $table->string('symbol');
+            $table->string('logo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('set');
+    }
+};
--- resources/views/set.blade.php
+++ resources/views/set.blade.php
@@ -28,7 +28,7 @@
         <!-- Displays the list of cards in the set -->
             <div class="card-list flex flex-wrap justify-between">
                 @foreach($cards as $card)
-                    <div class="card hover:scale-125 transition m-1 w-[calc(20%-10px)]">
+                    <div class="card hover:scale-110 transition m-1 w-[calc(20%-10px)]">
                             <a href="{{ route('card', ['id' => $card['id']]) }}">
                                 <img src="{{ $card['images']['small'] }}" class="rounded-2xl" style="box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.45);" alt="{{ $card['name'] }}">
                             </a>
