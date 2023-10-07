d38fd37ae07172464cf0a2de223d53ebe888e531
CAPEL Maxime <83071634+fortyup@users.noreply.github.com>
2023-10-07 21:48:25 +0200

    Add collection to header if connected

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,8 +8,11 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
+                <div class="px-6 pt-6 text-gray-900 dark:text-gray-100">
                     {{ __("You're logged in!") }}
+                </div>
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <a href="{{ route('index') }}" class="text-blue-500 hover:text-blue-700">Go to the website</a>
                 </div>
             </div>
         </div>
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -23,23 +23,28 @@
     @if (Route::has('login'))
         <div class="flex items-center mr-5">
             @auth
+                <!-- Dashboard -->
                 <a href="{{ url('/dashboard') }}"
-                   class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
+                   class="font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
+                <!-- Link to the collection -->
+                <a href="{{ url('/collection') }}"
+                   class="ml-4 font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">My
+                    collection</a>
             @else
                 <a href="{{ route('login') }}"
-                   class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log
+                   class="font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log
                     in</a>
 
                 @if (Route::has('register'))
                     <a href="{{ route('register') }}"
-                       class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
+                       class="ml-4 font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
                 @endif
             @endauth
         </div>
     @endif
 </header>
 
-<div class="container max-w-full p-12">
+<div class="container max-w-full max-h-auto p-12">
     @yield('content')
 </div>
 </body>
