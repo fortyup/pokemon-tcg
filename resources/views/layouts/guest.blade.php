0fed0db239b5a9fb4aefde36b3aee3b885419ade
CAPEL Maxime <83071634+fortyup@users.noreply.github.com>
2023-10-11 17:11:57 +0200

    change redirection from laravel dashboard to home page

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -17,8 +17,9 @@
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
             <div>
-                <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                <a href="/" class="flex flex-col items-center">
+                    <img src="{{ asset('images/800px-Feuforêve-RFVF.png') }}" alt="Pokemon logo" class="mr-2 ml-2 w-32">
+                    <h1 class="text-4xl">Pokémon TCG Collection</h1>
                 </a>
             </div>
 
