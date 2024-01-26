2e178de240829002ad8db131f5efe935046303c9
CAPEL Maxime <83071634+fortyup@users.noreply.github.com>
2024-01-26 10:09:49 +0100

    Fix rule display in card view

--- app/Models/Rule.php
+++ app/Models/Rule.php
@@ -9,15 +9,6 @@ class Rule extends Model
     protected $table = 'rule';
     protected $fillable = ['id', 'rule', 'card_id'];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'rule' => 'array',
-    ];
-
     public function card()
     {
         return $this->belongsTo(Card::class, 'card_id');
