1589cbed2716ff04c6fc5d5c8b21f25c39e2e97a
CAPEL Maxime <83071634+fortyup@users.noreply.github.com>
2024-02-11 18:18:13 +0100

    Mobile res for sets view

--- resources/views/sets.blade.php
+++ resources/views/sets.blade.php
@@ -9,20 +9,20 @@
         <h2 class="text-xl mb-3 mt-5 font-bold underline underline-offset-8">{{ $series }}</h2>
 
         <!-- Grid layout for sets within the series -->
-        <div class="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-3 gap-4 h-full">
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
             @foreach($seriesSets->sortByDesc('releaseDate') as $set)
                 <!-- Set card -->
                 <div class="bg-gray-100 rounded-lg shadow-md p-4 dark:bg-slate-700">
                     <a href="{{ route('set', ['set' => $set->id_set]) }}">
                         <!-- Set image -->
-                        <div class="h-36 flex w-full justify-center items-center">
+                        <div class="h-36 flex justify-center items-center">
                             <img src="{{ $set->logo }}" alt="{{ $set->name }}"
                                  class="max-w-full max-h-full justify-center flex">
                         </div>
                         <!-- Set details -->
-                        <div class="flex flex-row items-center p-4">
-                            <img src="{{ $set->symbol }}" alt="{{ $set->name }}" class="w-12 h-12 mr-2">
-                            <div class="flex-column">
+                        <div class="flex flex-col items-center p-4">
+                            <img src="{{ $set->symbol }}" alt="{{ $set->name }}" class="w-auto h-12 mb-2">
+                            <div class="text-center">
                                 <!-- Set name -->
                                 <p class="text-3xl">{{ $set->name }}</p>
                                 <!-- Release date -->
